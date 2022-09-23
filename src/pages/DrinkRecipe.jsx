4a80dcd8dab29259ed4dd448f6f344e32e0fbaf2
gabrielraeder <gabrielraeder@outlook.com.br>
2022-09-23 20:16:13 -0300

    requisito 12

--- src/pages/DrinkRecipe.jsx
+++ src/pages/DrinkRecipe.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Header from '../components/Header';
 
 export default function DrinkRecipe() {
   return (
     <div>
-      <Header title="DrinkRecipe" />
+      DrinkRecipe
     </div>
   );
 }
