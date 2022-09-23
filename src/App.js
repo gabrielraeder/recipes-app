2894e6976eac17e763c7f1265d26ba31f70f57c5
gabrielraeder <gabrielraeder@outlook.com.br>
2022-09-23 17:45:54 -0300

    requisitos 7 ao 9

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Provider from './context/Provider';
 import Login from './components/Login';
 import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+import MealRecipe from './pages/MealRecipe';
+import DrinkRecipe from './pages/DrinkRecipe';
+import DrinkInProgress from './pages/DrinkInProgress';
+import MealInProgress from './pages/MealInProgress';
+import Profile from './pages/Profile';
+import Done from './pages/Done';
+import Favorites from './pages/Favorites';
 
 function App() {
   return (
     <Provider>
       <div className="meals">
-        <span className="logo">TRYBE</span>
-        <object
-          className="rocksGlass"
-          type="image/svg+xml"
-          data={ rockGlass }
-        >
-          Glass
-        </object>
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route exact path="/meals" component={ Meals } />
+          <Route exact path="/drinks" component={ Drinks } />
+          <Route exact path="/meals/:id" component={ MealRecipe } />
+          <Route exact path="/drinks/:id" component={ DrinkRecipe } />
+          <Route exact path="/meals/:id/in-progress" component={ MealInProgress } />
+          <Route exact path="/drinks/:id/in-progress" component={ DrinkInProgress } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/done-recipes" component={ Done } />
+          <Route exact path="/favorite-recipes" component={ Favorites } />
         </Switch>
       </div>
     </Provider>
--- src/pages/MealInProgress.jsx
+++ src/pages/MealInProgress.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Header from '../components/Header';
 
-export default function Meals() {
+export default function MealInProgress() {
   return (
     <div>
-      <Header title="Meals" />
+      <Header title="MealInProgress" />
     </div>
   );
 }
