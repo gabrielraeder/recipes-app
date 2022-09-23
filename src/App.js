c4cbcf70fc492db5cb4065be859616954f9d062b
Marcielle Souza <marcielle9908@gmail.com>
2022-09-23 15:54:40 -0300

    Requisito 2 e 3 concluido.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Provider from './context/Provider';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -16,6 +18,9 @@ function App() {
         >
           Glass
         </object>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
       </div>
     </Provider>
   );
